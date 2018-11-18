20e4f95c9a1b9e9e4890b3a76d905e142972eece
lethesriver <45143083+lethesriver@users.noreply.github.com>
2018-11-18 21:17:18 +0900

    test

---
+++ 3_answer.py
@@ -0,0 +1,34 @@
+class Cal(object):
+    _history = []
+    def __init__(self, v1, v2):
+        if isinstance(v1, int):
+            self.v1 = v1
+        if isinstance(v2, int):
+            self.v2 = v2
+
+    def add(self):
+        result = self.v1+self.v2
+        Cal._history.append("add : %d+%d=%d" % (self.v1, self.v2, result))
+        return result
+    def subtract(self):
+        result = self.v1-self.v2
+        Cal._history.append("subtract : %d-%d=%d" % (self.v1, self.v2, result))
+        return result
+
+    @classmethod
+    def history(cls):
+        for item in Cal._history:
+            print(item)
+
+
+class CalMultiply(Cal):
+    def multiply(self):
+        result = self.v1*self.v2
+        Cal._history.append("multiply : %d*%d=%d" % (self.v1, self.v2, result))
+        return result
+
+
+c1 = CalMultiply(10,10)
+print(c1.add())
+print(c1.multiply())
+Cal.history()
